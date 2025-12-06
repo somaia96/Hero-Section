5550b5997ee47a9ccfb1493c3cd510c6f9835f95
somaia96 <somaia96.sh@gmail.com>
2025-12-06 15:55:33 +0300

    Edit The Text

--- src/component/MainLeftText.tsx
+++ src/component/MainLeftText.tsx
@@ -11,8 +11,8 @@ const MainLeftText = () => {
     }
     return (
         <div className='flex-1 flex flex-col justify-center items-center lg:items-start lg:pl-28 text-start space-y-5'>
-            <h1 className="text-3xl lg:text-5xl font-bold text-light-blue-900">MainHead Line</h1>
-            <h2 className="lg:text-xl text-center lg:text-left text-gray-600 w-3/4">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi fugit perspiciatis provident consectetur architecto! Repudiandae nihil quasi quaerat dolores sed, inventore in hic nam unde, aliquam nesciunt, at ipsum? Neque.</h2>
+            <h1 className="text-3xl lg:text-5xl font-bold text-light-blue-900">Your Cute Fish</h1>
+            <h2 className="lg:text-xl text-center lg:text-left text-gray-600 w-3/4">Buy A Cute Fish And Have A Free Care Program With The Right Tools And Food. Buy A Cute Fish And Have A Free Care Program With The Right Tools And Food. Visit Our Site To See Your Favourate Fish on Your Eyes .</h2>
             <div className="space-x-3 flex">
                 <Button
                     onClick={handleGetStarted}>
